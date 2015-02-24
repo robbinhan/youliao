3b9049050caea819e5a64e08f9d5a32da10f7104
robbinhan <luckyhanbin@gmail.com>
2015-02-24 15:16:31 +0800

    优化代码结构，添加子标题，每条新闻后增加发布时间

--- www/scripts/controllers.js
+++ www/scripts/controllers.js
@@ -1,18 +1,14 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope, $ionicModal, $timeout, Apps) {
+  $scope.menuItems = Apps.getConfig();
 })
-.controller('PlaylistsCtrl', function($scope, PlayLists, x2js, $window, $stateParams, 
-  $ionicLoading,$ionicActionSheet) {
+.controller('PlaylistsCtrl', function($scope, PlayLists, $window, $stateParams, 
+  $ionicLoading, Apps) {
+  var config = Apps.getConfig();
 
   var getPlayLists = function(type) {
-
-    //请求的rss类型
-    var playlistId = $stateParams.playlistId;
-
-    if (playlistId === undefined) {
-      playlistId = 'startupnews';
-    }
+    var playlistId = $stateParams.playlistId ? $stateParams.playlistId : 0;
 
     if (type == 'normal') {
       //显示loading层
@@ -26,8 +22,17 @@ angular.module('starter.controllers', [])
     }
 
     //获取文章列表
-    PlayLists.all(playlistId).then(function(data){
-      $scope.playlists = formatPlayLists(data,playlistId);
+    PlayLists.all(playlistId, config).then(function(data){
+      var menuitem = config[playlistId];
+      var filterType = menuitem.filterType;
+      $scope.subtitle = menuitem.name;
+      if (filterType == 0) {
+        $scope.playlists = PlayLists.rss2(data);
+      } else if (filterType == 1) {
+        $scope.playlists = PlayLists.feed(data);
+      } else {
+        $scope.playlists = PlayLists.feed2(data);
+      }
       if (type == 'normal') {
         $ionicLoading.hide();
       } else if (type == 'pulling') {
@@ -35,56 +40,6 @@ angular.module('starter.controllers', [])
         $scope.$broadcast('scroll.refreshComplete');
       }
     });
-  }
-
-  /**
-   * 组织整理get到的文章列表
-   * @param  {[type]} data [description]
-   * @return {[type]}      [description]
-   */
-  var formatPlayLists = function(data,playlistId) {
-    var playlists = [];
-    json_playlists = x2js.xml_str2json(data);
-    console.log(json_playlists);
-    //hacknews、startupnews、taobaoued、sinaued限制30条消息
-    if (playlistId == 'hacknews' || playlistId == 'startupnews'
-     || playlistId == 'dgtle' || playlistId == 'logdownweek') {
-      items = json_playlists.rss.channel.item
-      var len = items.length;
-      var item = {};
-      for (var i = 0; i < len; i++) {
-        if (i === 30) {
-          break;
-        }
-        item = items[i];
-        if (item.title != "") {
-          playlists.push({
-            id:item.link,
-            title:{__text:item.title},
-            published:item.pubDate
-          });
-        }
-      }
-    } else if (playlistId == 'next') {
-      entrys = json_playlists.feed.entry;
-      var len = entrys.length;
-      for (var i = 0; i < len; i++) {
-        if (i === 30) {
-          break;
-        }
-        item = entrys[i];
-        if (item.title != "") {
-          playlists.push({
-            id:item.link._href,
-            title:{__text:item.title},
-            published:item.published
-          });
-        }
-      }
-    } else {
-      playlists = json_playlists.feed.entry  
-    }
-    return playlists;
   }
 
   /**
@@ -113,49 +68,7 @@ angular.module('starter.controllers', [])
   }
 
   $scope.share = function(title, desc, url, thumb) {
-    $ionicActionSheet.show({
-        buttons: [
-            { text: '<b>分享至微信朋友圈</b>' },
-            { text: '分享给微信好友' }
-        ],
-        titleText: '分享',
-        cancelText: '取消',
-        cancel: function() {
-            // 取消时执行
-        },
-        buttonClicked: function(index) {
-            if(index == 0) {
-                $scope.shareViaWechat(WeChat.Scene.timeline, title, desc, url, thumb);
-            }
-            if(index ==1 ) {
-                $scope.shareViaWechat(WeChat.Scene.session, title, desc, url, thumb);
-            }
-        }
-    });
-  };
-
-  $scope.shareViaWechat = function(scene, title, desc, url, thumb) {
-    // 创建消息体
-    var msg = {
-        title: title ? title : "有料",
-        description: desc ? desc : "新闻聚合器",
-        url: url ? url : "http://fir.im/youliao",
-        thumb: thumb ? thumb : null
-    };
-
-    WeChat.share(msg, scene, function() {
-        $ionicPopup.alert({
-            title: '分享成功',
-            template: '感谢您的支持！',
-            okText: '关闭'
-        });
-    }, function(res) {
-        $ionicPopup.alert({
-            title: '分享失败',
-            template: '错误原因：' + res + '。',
-            okText: '我知道了'
-        });
-    });
+    PlayLists.share(title, desc, url, thumb);
   };
 
   getPlayLists('normal');
--- www/scripts/services.js
+++ www/scripts/services.js
@@ -1,23 +1,12 @@
 angular.module('starter.services', [])
 
-.factory('PlayLists', function($q,$http) {
+.factory('PlayLists', function($q, $http, x2js, $ionicActionSheet) {
   var playlists = {};
   var promise;
-  var config = {
-    'stackexchange':'http://stackexchange.com/feeds',
-    'segmentfault':'http://segmentfault.com/feeds',
-    'stackoverflow':'http://stackoverflow.com/feeds',
-    'hacknews':'https://news.ycombinator.com/rss',
-    'startupnews':'http://news.dbanotes.net/rss',
-    'guokr':'http://www.guokr.com/rss/',
-    'dgtle':'http://www.dgtle.com/rss/dgtle.xml',
-    'next':'http://next.36kr.com/feed',
-    'logdownweek':'http://feeds.feedburner.com/logdown/hot_week?format=xml'
-  };
-
-  playlists.all = function(playlistId){
+  
+  playlists.all = function(playlistId, config){
     var defer = $q.defer();
-    var url = config[playlistId];
+    var url = config[playlistId].url;
     $http.get(url).
     success(function(data, status, headers, config) {
       defer.resolve(data);
@@ -27,7 +16,124 @@ angular.module('starter.services', [])
     });
 
     return defer.promise;
-    
   }
+
+  playlists.share = function(title, desc, url, thumb){
+    $ionicActionSheet.show({
+        buttons: [
+          { text: '<b>分享至微信朋友圈</b>' },
+          { text: '分享给微信好友' }
+        ],
+        titleText: '分享',
+        cancelText: '取消',
+        cancel: function() {
+          // 取消时执行
+        },
+        buttonClicked: function(index) {
+          if(index == 0) {
+            shareViaWechat(WeChat.Scene.timeline, title, desc, url, thumb);
+          }
+          if(index ==1 ) {
+            shareViaWechat(WeChat.Scene.session, title, desc, url, thumb);
+          }
+        }
+    });
+  }
+
+  var shareViaWechat = function(scene, title, desc, url, thumb) {
+    // 创建消息体
+    var msg = {
+        title: title ? title : "有料",
+        description: desc ? desc : "新闻聚合器",
+        url: url ? url : "http://fir.im/youliao",
+        thumb: thumb ? thumb : null
+    };
+
+    WeChat.share(msg, scene, function() {
+        $ionicPopup.alert({
+            title: '分享成功',
+            template: '感谢您的支持！',
+            okText: '关闭'
+        });
+    }, function(res) {
+        $ionicPopup.alert({
+            title: '分享失败',
+            template: '错误原因：' + res + '。',
+            okText: '我知道了'
+        });
+    });
+  };
+
+  playlists.rss2 = function(data){
+    var playlists = [];
+    var json_playlists = x2js.xml_str2json(data);
+    var items = json_playlists.rss.channel.item
+    var len = items.length;
+    var item = {};
+    for (var i = 0; i < len; i++) {
+      if (i === 30) {
+        break;
+      }
+      item = items[i];
+      if (item.title != "") {
+        playlists.push({
+          id:item.link,
+          title:{__text:item.title},
+          published:item.pubDate
+        });
+      }
+    }
+    return playlists;
+  }
+
+  playlists.feed = function(data){
+    var playlists = [];
+    var json_playlists = x2js.xml_str2json(data);
+    var entrys = json_playlists.feed.entry;
+    var len = entrys.length;
+    for (var i = 0; i < len; i++) {
+      if (i === 30) {
+        break;
+      }
+      item = entrys[i];
+      if (item.title != "") {
+        playlists.push({
+          id:item.link._href,
+          title:{__text:item.title},
+          published:item.published
+        });
+      }
+    }
+    return playlists;
+  }
+
+  playlists.feed2 = function(data){
+    var json_playlists = x2js.xml_str2json(data);
+    return json_playlists.feed.entry;
+  }
+
   return playlists;
+})
+
+.factory('Apps', function() {
+
+  var apps = {};
+
+  var config = [
+    {'url':'https://news.ycombinator.com/rss','name':'HackNews','ico':'hacknews.ico','filterType':0},
+    {'url':'http://news.dbanotes.net/rss','name':'StartupNews','ico':'startupnews.ico','filterType':0},
+    {'url':'http://www.guokr.com/rss/','name':'果壳','ico':'guokr.png','filterType':2},
+    {'url':'http://www.dgtle.com/rss/dgtle.xml','name':'数字尾巴','ico':'dgtle.ico','filterType':0},
+    {'url':'http://next.36kr.com/feed','name':'Next','ico':'next.ico','filterType':1},
+    {'url':'http://feeds.feedburner.com/logdown/hot_week?format=xml','name':'LogDown Week','ico':'logdown.jpg','filterType':0},
+    {'url':'http://stackexchange.com/feeds','name':'StackExchange','ico':'stackexchange.ico','filterType':2},
+    {'url':'http://segmentfault.com/feeds','name':'SegmentFault','ico':'segmentfault.ico','filterType':2},
+    {'url':'http://stackoverflow.com/feeds','name':'StackoverFlow','ico':'stackoverflow.ico','filterType':2}
+  ];
+
+  apps.getConfig = function(){
+    return config;
+  }
+
+  return apps;
 });
